17eef6d30d458cc9731559de03b8991b587954e9
Korolev Aleksandr <sanyastronger@rambler.ru>
2024-02-14 12:15:05 +0300

    домашняя работа 6

--- src/video.py
+++ src/video.py
@@ -10,13 +10,21 @@ class Video:
     def __init__(self, video_id):
         self.video_id = video_id
         self.video = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails', id=video_id).execute()
-        self.name = self.video['items'][0]["snippet"]['title']
-        self.url = f'https://www.youtube.com/{self.video_id}'
-        self.quantity_view = self.video['items'][0]['statistics']['viewCount']
-        self.quantity_like = self.video['items'][0]['statistics']['likeCount']
+        try:
+            self.title = self.video['items'][0]["snippet"]['title']
+            self.url = f'https://www.youtube.com/{self.video_id}'
+            self.quantity_view = self.video['items'][0]['statistics']['viewCount']
+            self.like_count = self.video['items'][0]['statistics']['likeCount']
+        except IndexError:
+            print(IndexError('несуществующий id'))
+        finally:
+            self.title = None
+            self.url = None
+            self.quantity_view = None
+            self.like_count = None
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
 class PLVideo(Video):
